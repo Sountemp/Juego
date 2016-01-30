aec6fb2c3dd17935dae33732700ff9e173c3bf7d
Marcos Phelipe Stival Tome <markcosta15@hotmail.com>
2016-01-30 02:05:21 +0100

    interfaz añadida. toggles funcionan bien.

---
+++ Assets/Scripts/AnswerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class AnswerScript : MonoBehaviour {
+
+	GameObject[] answersToggle;
+
+	// Use this for initialization
+	void Start () {
+		answersToggle = GameObject.FindGameObjectsWithTag ("Answer");
+		foreach (GameObject answer in answersToggle) {
+			Debug.Log ("hey");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
