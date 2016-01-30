b77fe142badb078e6a1869ab3e8373d6712733d1
Marcos Phelipe Stival Tome <markcosta15@hotmail.com>
2016-01-30 10:36:15 +0100

    Commit con json leyendo

---
+++ Assets/Scripts/ReadScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class ReadScript : MonoBehaviour {
+
+	public TextAsset jsonFile;
+
+	private string escena;
+	JSONNode N;
+	// Use this for initialization
+	void Start()
+	{
+		N = JSONNode.Parse (jsonFile.text);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		string a = N ["Mensajes"] [0] ["Personaje"];
+		Debug.Log (N["Mensajes"].Count);
+ 		GetPersonaje ();
+
+	}
+	public void GetPersonaje(){
+		for (int i = 0; i < N ["Mensajes"].Count; i++) {
+			if (N ["Mensajes"] [i] == "Personaje") {
+				Debug.Log (N ["Mensajes"] [i]);
+			}
+		}
+	}
+}
