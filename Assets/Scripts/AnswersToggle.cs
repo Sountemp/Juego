0777359cb76f351f88d090d3a46db2ed80cf594f
Marcos Phelipe Stival Tome <markcosta15@hotmail.com>
2016-01-30 16:04:11 +0100

    La interfaz 'funciona'

---
+++ Assets/Scripts/AnswersToggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnswersToggle : MonoBehaviour {
+
+	ReadScript rs;
+	GameObject answers;
+ 	// Use this for initialization
+	void Start () {
+		rs = GetComponent<ReadScript> ();
+		answers = GameObject.Find ("Answers");
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+	}
+	public void UpdateOnToggle(bool ev){
+		//Comprueba si la respuesta es correctas
+ 		rs.contPreg ++;
+
+	}
+	private void ClearToggles(){
+		ToggleGroup tg =answers.GetComponent<ToggleGroup> ();
+		tg.SetAllTogglesOff ();
+	}
+}
